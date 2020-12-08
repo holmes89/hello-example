61fec3689f8cc0470a68e55b7f2f042a99aa234c
Joel Holmes <holmes89@gmail.com>
2020-12-08 12:40:29 -0500

    updated pipeline

---
+++ ci/lint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+go get -t ./...
+go vet ./...
+go get -u golang.org/x/lint/golint
+go install golang.org/x/lint/golint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.33.0
+curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.5.0
+
+golint ./...
+golangci-lint run
+gosec ./...
