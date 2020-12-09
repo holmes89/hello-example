4af531268a735b0aef0d8e9e06bfbc0354bb77b8
Joel Holmes <holmes89@gmail.com>
2020-12-09 09:15:45 -0500

    serverless deployment

---
+++ ci/terraform-deploy-serverless.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Deploying Lambda"
+cd hello-code/terraform/prod/services/serverless
+
+export VERSION=$(git rev-parse --short HEAD)
+export FILENAME="releases/server-$VERSION.zip"
+
+terraform init
+terraform apply -auto-approve -var "file_name=$FILENAME"
